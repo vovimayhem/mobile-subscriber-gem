0b14cc641ca405f8e5554885d3c82645c205a951
Roberto Quintanilla <roberto.quintanilla@naranya.com>
2015-03-25 09:18:58 -0600

    Updated Claro Peru request sample

--- spec/factories/requests.rb
+++ spec/factories/requests.rb
@@ -6,8 +6,8 @@ FactoryGirl.define do
     initialize_with do
       {
         "HTTP_HOST"             =>  "www.example.com",
-        "HTTP_CONNECTION"       =>  "keep-alive",
-        "HTTP_CACHE_CONTROL"    =>  "max-age=0",
+        "HTTP_CONNECTION"       =>  "close",
+        "HTTP_CACHE_CONTROL"    =>  "no-cache, max-age=0",
         "HTTP_ACCEPT"           =>  "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
         "HTTP_USER_AGENT"       =>  "Mozilla/5.0 (iPhone; CPU iPhone OS 7_0 like Mac OS X; en-us) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53",
         "HTTP_ACCEPT_ENCODING"  =>  "gzip, deflate, sdch",
@@ -163,13 +163,34 @@ FactoryGirl.define do
       end
     end
 
-    # http://myip.ms/view/ip_owners/90291/America_Movil_Peru_S_a_c.html
+    # Real request recorded at claroapps.com at 2015-03-24
     factory :mobile_request_from_claro_peru do
       initialize_with do
         new build(:common_mobile_request_env).merge(
-          "REMOTE_ADDR"               => "200.108.96.5",
-          "HTTP_X_UP_CALLING_LINE_ID" => "5116130000",
-          "HTTP_VIA"                  => "aa Comverse aaa"
+          "REMOTE_ADDR"             => "216.246.75.208", # This IP belongs to a U.S. datacenter which acts as proxy for the real user...
+
+          "HTTP_ACCEPT_LANGUAGE"    => "es-PE,es-419;q=0.8,es;q=0.6",
+          "HTTP_AKAMAI_ORIGIN_HOP"  => "1",
+          "HTTP_CONNECT_TIME"       => "3",
+          "HTTP_PRAGMA"             => "no-cache",
+          "HTTP_TOTAL_ROUTE_TIME"   => "5",
+          "HTTP_VERSION"            => "HTTP/1.1",
+          "HTTP_VIA"                => "1.1 akamai.net(ghost) (AkamaiGHost), 1.1 vegur",
+
+          # X Headers:
+          "HTTP_X_AKAMAI_CONFIG_LOG_DETAIL" => "true",
+
+          # The leftmost IP is the:
+          # - Closer to the User (or the actual user IP)
+          # - Less trustable
+          "HTTP_X_FORWARDED_FOR"      => "190.113.210.147, 216.246.75.208",
+          "HTTP_X_NOKIA_IMSI"         => "716100316259449",
+          "HTTP_X_REQUEST"            => "272aa345-55cc-45b2-b879-79678957817f",
+
+          "HTTP_X_REQUEST_START"      => "1427225820026",
+          "HTTP_X_FORWARDED_PORT"     => "80",
+          "HTTP_X_FORWARDED_PROTO"    => "http",
+          "HTTP_X_UP_CALLING_LINE_ID" => "51941037249"
         )
       end
     end
